ef898ee0fb702dae0b875a41a058dbdd4eff3d56
Phil Crilley <philc@youronlinemarketplace.com>
2022-01-03 20:58:02 -0800

    Initial setup

--- bestbuy_checkout.py
+++ bestbuy_checkout.py
@@ -1,3 +1,29 @@
 from helium import *
+import chromedriver_autoinstaller
+import pyotp
 
-start_chrome()
+import proxy_loader
+
+BESTBUY_2FA_TOKEN = "QW2LN7JNEHEVV65Y"
+bestbuy_2fa_code = pyotp.TOTP(BESTBUY_2FA_TOKEN)
+
+# Check if the current version of chromedriver exists
+# and if it doesn't exist, download it automatically,
+# then add chromedriver to path
+chromedriver_autoinstaller.install()
+
+
+chrome_options = proxy_loader.get_chromedriver(use_proxy=True)
+chrome_options.add_extension(
+    '~/git_repos/bestbuy-queue-cracker.crx')
+chrome_options.add_extension(
+    'NerdSpeak_Stock_Helper_1.0.0.0.crx')
+
+# chrome_options.add_argument(
+#     "--proxy-server={}".format(PROXY))
+
+"button.c-close-icon.c-modal-close-icon"
+
+sku_page = "https://www.bestbuy.com/site/pny-nvidia-geforce-rtx-3060-12gb-xlr8-gaming-revel-epic-x-rgb-single-fan-graphics-card/6454318.p?skuId=6454318"
+
+driver = start_chrome(sku_page, options=chrome_options)
---
+++ proxy_loader.py
@@ -0,0 +1,76 @@
+import os
+import zipfile
+
+from selenium.webdriver.chrome.options import Options
+
+
+PROXY_USER = "coiaacye"
+PROXY_PASS = "kkul1ixr4jnp"
+PROXY_HOST = "142.111.248.163"
+PROXY_PORT = 20000
+
+manifest_json = """
+{
+    "version": "1.0.0",
+    "manifest_version": 2,
+    "name": "Chrome Proxy",
+    "permissions": [
+        "proxy",
+        "tabs",
+        "unlimitedStorage",
+        "storage",
+        "<all_urls>",
+        "webRequest",
+        "webRequestBlocking"
+    ],
+    "background": {
+        "scripts": ["background.js"]
+    },
+    "minimum_chrome_version":"22.0.0"
+}
+"""
+
+background_js = """
+var config = {
+        mode: "fixed_servers",
+        rules: {
+          singleProxy: {
+            scheme: "http",
+            host: "%s",
+            port: parseInt(%s)
+          },
+          bypassList: ["localhost"]
+        }
+      };
+
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "%s",
+            password: "%s"
+        }
+    };
+}
+
+chrome.webRequest.onAuthRequired.addListener(
+            callbackFn,
+            {urls: ["<all_urls>"]},
+            ['blocking']
+);
+""" % (PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS)
+
+
+def get_chromedriver(use_proxy=False, user_agent=None):
+    chrome_options = Options()
+    if use_proxy:
+        pluginfile = 'proxy_auth_plugin.zip'
+
+        with zipfile.ZipFile(pluginfile, 'w') as zp:
+            zp.writestr("manifest.json", manifest_json)
+            zp.writestr("background.js", background_js)
+        chrome_options.add_extension(pluginfile)
+    if user_agent:
+        chrome_options.add_argument('--user-agent=%s' % user_agent)
+    return chrome_options
