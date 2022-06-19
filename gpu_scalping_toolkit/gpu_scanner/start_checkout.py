4c7bd7664cef1a5ff0cd27c3c8172cc739974a07
philip06 <pcrilley06@gmail.com>
2022-06-19 14:11:21 -0400

    Update start_checkout.py

--- gpu_scalping_toolkit/gpu_scanner/start_checkout.py
+++ gpu_scalping_toolkit/gpu_scanner/start_checkout.py
@@ -9,37 +9,15 @@ from gpu_scalping_toolkit.gpu_checkout_bot.account import ACCOUNT_REGION, Accoun
 
 def startCheckout(sku):
     accounts: list = [{
-        "email": "bbverify88@gmail.com",
-        "password": "w9P4mfz7UY6N2S",
-        "gmail_password": "w9P4mfz7UY6N2S",
-        "2fa_token": "4YQJH2QGRIDMXRPD",
+        "email": "",
+        "password": "",
+        "gmail_password": "",
+        "2fa_token": "",
         "region": ACCOUNT_REGION.DMV,
-        "proxy_user": "coiaacye",
-        "proxy_password": "kkul1ixr4jnp",
-        "proxy_host": "23.230.25.103",
-        "proxy_port": 20000
-    },
-        {
-        "email": "hkapperbb@gmail.com",
-        "password": "EyujcSv8FbuP6ASs",
-        "gmail_password": "EyujcSv8FbuP6ASs",
-        "2fa_token": "MI64KYY5QLQH62X3",
-        "region": ACCOUNT_REGION.DMV,
-        "proxy_user": "coiaacye",
-        "proxy_password": "kkul1ixr4jnp",
-        "proxy_host": "23.230.25.104",
-        "proxy_port": 20000
-    },
-        {
-        "email": "kapsakcjvv@gmail.com",
-        "password": "6y8B7^y!C4$7",
-        "gmail_password": "U7d7#xCVL4Qe0!pu",
-        "2fa_token": "O5FK557TKTLZCSOJ",
-        "region": ACCOUNT_REGION.ATL,
-        "proxy_user": "coiaacye",
-        "proxy_password": "kkul1ixr4jnp",
-        "proxy_host": "23.230.25.105",
-        "proxy_port": 20000
+        "proxy_user": "",
+        "proxy_password": "",
+        "proxy_host": "",
+        "proxy_port": 0
     }]
 
     pool = Pool()
